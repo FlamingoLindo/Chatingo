b19235fbcaf54b7a2040159ec8c3cf1ca9dd99de
FlamingoLindo <vitorantunes2003@gmail.com>
2026-02-17 14:19:26 -0300

    fix: update README workflow to trigger on all branches and improve commit fetching logic

--- src/routes/emote-tab/+page.svelte
+++ src/routes/emote-tab/+page.svelte
@@ -14,7 +14,10 @@
 
 <main class="flex flex-col h-full bg-black text-white">
   <div class="flex-1 overflow-auto p-4">
-    <p class="text-gray-400">Search...</p>
+    <input
+      class="w-full text-gray-400 border border-[#ffc9c9] bg-[#111111] p-1 focus:outline-none focus:ring-0"
+      placeholder="Search..."
+    />
     <EmotesTypes />
   </div>
 </main>
