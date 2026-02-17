c7aec301f381fba4afd070d52082290fd3545d1f
FlamingoLindo <vitorantunes2003@gmail.com>
2026-02-17 09:53:41 -0300

    refactor: user card component for improved readability and add close button functionality

--- src/lib/components/Main/User_card/User_card.svelte
+++ src/lib/components/Main/User_card/User_card.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
   import { openUrl } from '@tauri-apps/plugin-opener';
-  let { isOpen = $bindable(), channel, x, y }: { isOpen: boolean | string | null; channel: string; x: number; y: number } = $props();
+  let {
+    isOpen = $bindable(),
+    channel,
+    x,
+    y,
+  }: {
+    isOpen: boolean | string | null;
+    channel: string;
+    x: number;
+    y: number;
+  } = $props();
 
   async function openTwitch(channel: string) {
     await openUrl(`https://www.twitch.tv/${channel}`);
@@ -17,30 +27,32 @@
   let isDragging = $state(false);
   let dragOffset = $state({ x: 0, y: 0 });
   let position = $state({ x: 0, y: 0 });
-  
+
   const cardWidth = 320;
   const cardHeight = 144;
-  
+
   let initialPosition = $derived.by(() => {
     const proposedX = x;
     const proposedY = y - 150;
-    
+
     const maxX = window.innerWidth - cardWidth;
     const maxY = window.innerHeight - cardHeight;
-    
+
     const boundedX = Math.max(0, Math.min(proposedX, maxX));
     const boundedY = Math.max(0, Math.min(proposedY, maxY));
-    
+
     return { x: boundedX, y: boundedY };
   });
-  
-  let cardStyle = $derived(`left: ${position.x}px; top: ${position.y}px; cursor: ${isDragging ? 'grabbing' : 'grab'};`);
+
+  let cardStyle = $derived(
+    `left: ${position.x}px; top: ${position.y}px; cursor: ${isDragging ? 'grabbing' : 'grab'};`
+  );
 
   function handleMouseDown(event: MouseEvent) {
     isDragging = true;
     dragOffset = {
       x: event.clientX - position.x,
-      y: event.clientY - position.y
+      y: event.clientY - position.y,
     };
   }
 
@@ -50,10 +62,10 @@
       const cardHeight = 144; // h-36
       const maxX = window.innerWidth - cardWidth;
       const maxY = window.innerHeight - cardHeight;
-      
+
       const newX = Math.max(0, Math.min(event.clientX - dragOffset.x, maxX));
       const newY = Math.max(0, Math.min(event.clientY - dragOffset.y, maxY));
-      
+
       position = { x: newX, y: newY };
     }
   }
@@ -86,7 +98,6 @@
 </script>
 
 {#if isOpen}
-  <!-- Card -->
   <div
     class="fixed w-80 h-36 bg-[#111111] text-white flex items-center gap-8 p-4 border border-[#ffc9c9] z-9999 shadow-2xl"
     style={cardStyle}
@@ -126,5 +137,23 @@
       <p class="text-sm">Followers: 912049292</p>
       <p class="text-sm">Created at: 1808-11-12</p>
     </div>
+
+    <button
+      class="absolute top-2 right-2 cursor-pointer hover:bg-[#ffffff36]"
+      aria-label="close_user_card"
+      onclick={() => (isOpen = !isOpen)}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="15"
+        height="15"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="#ffffff"
+          d="M13.46 12L19 17.54V19h-1.46L12 13.46L6.46 19H5v-1.46L10.54 12L5 6.46V5h1.46L12 10.54L17.54 5H19v1.46z"
+        />
+      </svg>
+    </button>
   </div>
 {/if}
